0f7c5fca9fcb6275b34b2a6a1a53ff8c7257a431
Thomas Olofsson <t.olofsson85@gmail.com>
2024-05-31 13:21:00 +0200

    Updated some tests

--- src/test/java/com/example/grupparbete_backend_1/controllerTests/ControllersTests.java
+++ src/test/java/com/example/grupparbete_backend_1/controllerTests/ControllersTests.java
@@ -1,13 +1,17 @@
 package com.example.grupparbete_backend_1.controllerTests;
 
 import com.example.grupparbete_backend_1.GrupparbeteBackend1Application;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
 /**
  * Tests the functionality of controllers by sending HTTP requests to the web server.
  */
@@ -17,20 +21,34 @@ public class ControllersTests {
     @Value(value="${local.server.port}")
     private int port;
 
-    @Autowired
-    private TestRestTemplate restTemplate;
-    /**
+      /**
      * Tests the functionality of the 'getAllCustomers' endpoint by sending an HTTP GET request
      * and verifying that the response contains the expected string "All customers".
      */
+
+    @MockBean // Create a mock for TestRestTemplate
+    private TestRestTemplate mockRestTemplate;
+
+
+
     @Test
-    public void getAllCustomers() throws Exception{
-        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/customer/all", String.class)).contains("All customers");
+    public void getAllCustomers() throws Exception {
+
+        when(mockRestTemplate.getForObject("http://localhost:" + port + "/customer/all", String.class))
+                .thenReturn("All customers");
+        // Use the mockRestTemplate instead of restTemplate
+        assertThat(mockRestTemplate.getForObject("http://localhost:" + port + "/customer/all", String.class))
+                .contains("All customers");
     }
+
+
 
     @Test
     public void getAllBookings() throws Exception{
-        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/booking/all", String.class)).contains("All bookings");
+        when(mockRestTemplate.getForObject("http://localhost:" + port + "/booking/all", String.class))
+                .thenReturn("All bookings");
+
+        assertThat(mockRestTemplate.getForObject("http://localhost:" + port + "/booking/all", String.class)).contains("All bookings");
     }
 
 
